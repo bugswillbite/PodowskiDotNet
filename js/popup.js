d76aa1f6bb8822edadc1da8ede51ba654a3c4c24
bella podowski <143750067+bugswillbite@users.noreply.github.com>
2025-07-03 09:33:07 -0500

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,75 @@
+// document.getElementById("openPopup").onclick = function () {
+//   document.getElementById("popup").style.display = "block";
+// };
+
+// document.getElementById("closePopup").onclick = function () {
+//   document.getElementById("popup").style.display = "none";
+// };
+
+// // Optional: close popup when clicking outside the content
+// window.onclick = function (event) {
+//   const popup = document.getElementById("popup");
+//   if (event.target == popup) {
+//     popup.style.display = "none";
+//   }
+// };
+
+document.getElementById("openPopup").onclick = function () 
+{
+  window.open
+  (
+    "https://bugswillbite.github.io/rUsmart/",
+    "rusmartWindow",
+    "width=800,height=500,resizable=yes,scrollbars=yes"
+  );
+};
+
+document.getElementById("openPopup2").onclick = function () 
+{
+  window.open
+  (
+    "https://bugswillbite.github.io/garfield/",
+    "garfield",
+    "width=1000,height=1000,top=20,left=200,resizable=yes,scrollbars=yes"
+  );
+};
+
+document.getElementById("openPopup3").onclick = function () 
+{
+  window.open
+  (
+    "../intrabarf.html",
+    "intraBarf",
+    "width=1100,height=600,top=20,left=200,resizable=yes,scrollbars=yes"
+  );
+};
+
+document.getElementById("openPopup4").onclick = function () 
+{
+  window.open
+  (
+    "https://bugswillbite.itch.io/mysa",
+    "mysa",
+    "width=800,height=700,top=220,left=600,resizable=yes,scrollbars=yes"
+  );
+};
+
+document.getElementById("openPopup5").onclick = function () 
+{
+  window.open
+  (
+    "../desktop.html",
+    "desktop",
+    "width=1000,height=600,top=20,left=200,resizable=yes,scrollbars=yes"
+  );
+};
+
+document.getElementById("openPopup6").onclick = function () 
+{
+  window.open
+  (
+    "https://bugswillbite.github.io/BigBugsBrain/",
+    "rusmartWindow",
+    "width=1000,height=1000,top=20,left=200,resizable=yes,scrollbars=yes"
+  );
+};
